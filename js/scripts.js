c614066cd92b4f5679341944424d9ca9d86278fa
Sara Schultz and Shane Stafford <shane@>
2017-02-14 16:36:13 -0800

    solidify all toggling functions.

--- js/scripts.js
+++ js/scripts.js
@@ -3,76 +3,76 @@ $(document).ready(function() {
     $("#js-hide").toggle();
     $("#js-show").toggle();
   });
-//   $(".col-md-3").click(function() {
-//     $("#op-show").toggle();
-//     $("#op-hide").toggle();
-//   });
-//   $(".col-md-3").click(function() {
-//     $("#var-show").toggle();
-//     $("#var-hide").toggle();
-//   });
-//   $(".col-md-3").click(function() {
-//     $("#vnc-show").toggle();
-//     $("#vnc-hide").toggle();
-//   });
-//   $(".col-md-3").click(function() {
-//     $("#fn-show").toggle();
-//     $("#fn-hide").toggle();
-//   });
-//   $(".col-md-3").click(function() {
-//     $("#meth-show").toggle();
-//     $("#meth-hide").toggle();
-//   });
-//   $(".col-md-3").click(function() {
-//     $("#arg-show").toggle();
-//     $("#arg-hide").toggle();
-//   });
-//   $(".col-md-3").click(function() {
-//     $("#par-show").toggle();
-//     $("#par-hide").toggle();
-//   });
-//   $(".col-md-3").click(function() {
-//     $("#re-show").toggle();
-//     $("#re-hide").toggle();
-//   });
-//   $(".col-md-3").click(function() {
-//     $("#chain-show").toggle();
-//     $("#chain-hide").toggle();
-//   });
-//   $(".col-md-3").click(function() {
-//     $("#string-show").toggle();
-//     $("#string-hide").toggle();
-//   });
-//   $(".col-md-3").click(function() {
-//     $("#boo-show").toggle();
-//     $("#boo-hide").toggle();
-//   });
-//   $(".col-md-3").click(function() {
-//     $("#unde-show").toggle();
-//     $("#unde-hide").toggle();
-//   });
-//   $(".col-md-3").click(function() {
-//     $("#nan-show").toggle();
-//     $("#nan-hide").toggle();
-//   });
-//   $(".col-md-3").click(function() {
-//     $("#esc-show").toggle();
-//     $("#esc-hide").toggle();
-//   });
-//   $(".col-md-3").click(function() {
-//     $("#alert-show").toggle();
-//     $("#alert-hide").toggle();
-//   });
-//   $(".col-md-3").click(function() {
-//     $("#com-show").toggle();
-//     $("#com-hide").toggle();
-//   });
-//   $(".col-md-3").click(function() {
-//     $("#jq-show").toggle();
-//     $("#jq-hide").toggle();
-//   });
-//   $(".col-md-3").click(function() {
-//     $("#att-show").toggle();
-//     $("#att-hide").toggle();
-//   });
+  $(".op").click(function() {
+    $("#op-show").toggle();
+    $("#op-hide").toggle();
+  });
+  $(".var").click(function() {
+    $("#var-show").toggle();
+    $("#var-hide").toggle();
+  });
+  $(".vnc").click(function() {
+    $("#vnc-show").toggle();
+    $("#vnc-hide").toggle();
+  });
+  $(".fn").click(function() {
+    $("#fn-show").toggle();
+    $("#fn-hide").toggle();
+  });
+  $(".meth").click(function() {
+    $("#meth-show").toggle();
+    $("#meth-hide").toggle();
+  });
+  $(".arg").click(function() {
+    $("#arg-show").toggle();
+    $("#arg-hide").toggle();
+  });
+  $(".par").click(function() {
+    $("#par-show").toggle();
+    $("#par-hide").toggle();
+  });
+  $(".re").click(function() {
+    $("#re-show").toggle();
+    $("#re-hide").toggle();
+  });
+  $(".chain").click(function() {
+    $("#chain-show").toggle();
+    $("#chain-hide").toggle();
+  });
+  $(".string").click(function() {
+    $("#string-show").toggle();
+    $("#string-hide").toggle();
+  });
+  $(".boo").click(function() {
+    $("#boo-show").toggle();
+    $("#boo-hide").toggle();
+  });
+  $(".unde").click(function() {
+    $("#unde-show").toggle();
+    $("#unde-hide").toggle();
+  });
+  $(".nan").click(function() {
+    $("#nan-show").toggle();
+    $("#nan-hide").toggle();
+  });
+  $(".esc").click(function() {
+    $("#esc-show").toggle();
+    $("#esc-hide").toggle();
+  });
+  $(".alert").click(function() {
+    $("#alert-show").toggle();
+    $("#alert-hide").toggle();
+  });
+  $(".com").click(function() {
+    $("#com-show").toggle();
+    $("#com-hide").toggle();
+  });
+  $(".jq").click(function() {
+    $("#jq-show").toggle();
+    $("#jq-hide").toggle();
+  });
+  $(".att").click(function() {
+    $("#att-show").toggle();
+    $("#att-hide").toggle();
+  });
 });
