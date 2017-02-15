db0c0ec6260c019dafa38526e88456695e3b9870
Sara Schultz and Shane Stafford <sara@>
2017-02-14 16:08:34 -0800

    add IDs, initial toggle functionality

---
+++ js/scripts.js
@@ -0,0 +1,78 @@
+$(document).ready(function() {
+  $(".js").click(function() {
+    $("#js-hide").toggle();
+    $("#js-show").toggle();
+  });
+//   $(".col-md-3").click(function() {
+//     $("#op-show").toggle();
+//     $("#op-hide").toggle();
+//   });
+//   $(".col-md-3").click(function() {
+//     $("#var-show").toggle();
+//     $("#var-hide").toggle();
+//   });
+//   $(".col-md-3").click(function() {
+//     $("#vnc-show").toggle();
+//     $("#vnc-hide").toggle();
+//   });
+//   $(".col-md-3").click(function() {
+//     $("#fn-show").toggle();
+//     $("#fn-hide").toggle();
+//   });
+//   $(".col-md-3").click(function() {
+//     $("#meth-show").toggle();
+//     $("#meth-hide").toggle();
+//   });
+//   $(".col-md-3").click(function() {
+//     $("#arg-show").toggle();
+//     $("#arg-hide").toggle();
+//   });
+//   $(".col-md-3").click(function() {
+//     $("#par-show").toggle();
+//     $("#par-hide").toggle();
+//   });
+//   $(".col-md-3").click(function() {
+//     $("#re-show").toggle();
+//     $("#re-hide").toggle();
+//   });
+//   $(".col-md-3").click(function() {
+//     $("#chain-show").toggle();
+//     $("#chain-hide").toggle();
+//   });
+//   $(".col-md-3").click(function() {
+//     $("#string-show").toggle();
+//     $("#string-hide").toggle();
+//   });
+//   $(".col-md-3").click(function() {
+//     $("#boo-show").toggle();
+//     $("#boo-hide").toggle();
+//   });
+//   $(".col-md-3").click(function() {
+//     $("#unde-show").toggle();
+//     $("#unde-hide").toggle();
+//   });
+//   $(".col-md-3").click(function() {
+//     $("#nan-show").toggle();
+//     $("#nan-hide").toggle();
+//   });
+//   $(".col-md-3").click(function() {
+//     $("#esc-show").toggle();
+//     $("#esc-hide").toggle();
+//   });
+//   $(".col-md-3").click(function() {
+//     $("#alert-show").toggle();
+//     $("#alert-hide").toggle();
+//   });
+//   $(".col-md-3").click(function() {
+//     $("#com-show").toggle();
+//     $("#com-hide").toggle();
+//   });
+//   $(".col-md-3").click(function() {
+//     $("#jq-show").toggle();
+//     $("#jq-hide").toggle();
+//   });
+//   $(".col-md-3").click(function() {
+//     $("#att-show").toggle();
+//     $("#att-hide").toggle();
+//   });
+});
